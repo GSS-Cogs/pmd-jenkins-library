f9b4490f77743cf6adbcf1949d2c5baf97e28839
Alex Tucker <alex@floop.org.uk>
2018-11-08 10:45:54 +0000

    Factor out components and codelists table2qb pipelines.

---
+++ vars/uploadComponents.groovy
@@ -0,0 +1,15 @@
+def call(String csv) {
+    configFileProvider([configFile(fileId: 'pmd', variable: 'configfile')]) {
+        def config = readJSON(text: readFile(file: configfile))
+        String PMD = config['pmd_api']
+        String credentials = config['credentials']
+        String PIPELINE = config['pipeline_api']
+        String baseURI = config['base_uri']
+
+        def draft = jobDraft.find()
+
+        runPipeline("${PIPELINE}/ons-table2qb.core/components/import",
+                draft.id, credentials, [[name: 'components-csv',
+                                         file: [name: csv, type: 'text/csv']]])
+    }
+}
