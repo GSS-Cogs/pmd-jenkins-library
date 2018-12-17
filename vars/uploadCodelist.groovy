a2e05b0886617515e6dafbbfbc290c2e6b03150f
Alex Tucker <alex@floop.org.uk>
2018-12-17 14:19:35 +0000

    British spelling of slugize :(

--- vars/uploadCodelist.groovy
+++ vars/uploadCodelist.groovy
@@ -3,6 +3,6 @@ import uk.org.floop.jenkins_pmd.PMD
 def call(String csv, String name) {
     PMD pmd = pmdConfig('pmd')
     def draft = jobDraft.find()
-    pmd.drafter.deleteGraph(draft.id as String, "${pmd.config.base_uri}/graph/${util.slugize(name)}")
+    pmd.drafter.deleteGraph(draft.id as String, "${pmd.config.base_uri}/graph/${util.slugise(name)}")
     pmd.pipelines.codelist(draft.id as String, csv, name)
 }
