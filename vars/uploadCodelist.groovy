59ce786f5495d91bfe0be05f735b3719d651e5d6
Alex Tucker <alex@floop.org.uk>
2018-12-17 14:27:41 +0000

    Use WORKSPACE env variable to find file to upload.

--- vars/uploadCodelist.groovy
+++ vars/uploadCodelist.groovy
@@ -4,5 +4,5 @@ def call(String csv, String name) {
     PMD pmd = pmdConfig('pmd')
     def draft = jobDraft.find()
     pmd.drafter.deleteGraph(draft.id as String, "${pmd.config.base_uri}/graph/${util.slugise(name)}")
-    pmd.pipelines.codelist(draft.id as String, csv, name)
+    pmd.pipelines.codelist(draft.id as String, "${WORKSPACE}/${csv}", name)
 }
