74eca1533cfa548ab8196be54e077c0083b762d7
Alex Tucker <alex@floop.org.uk>
2018-12-21 09:45:48 +0000

    Prefix file with workspace directory.

--- vars/uploadComponents.groovy
+++ vars/uploadComponents.groovy
@@ -3,5 +3,5 @@ import uk.org.floop.jenkins_pmd.PMD
 def call(String csv) {
     PMD pmd = pmdConfig('pmd')
     def draft = jobDraft.find()
-    pmd.pipelines.components(draft.id as String, csv)
+    pmd.pipelines.components(draft.id as String, "${WORKSPACE}/${csv}")
 }
