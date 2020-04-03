8a07b93e4126554abb616c8ad4757caefa376068
Alex Tucker <alex@floop.org.uk>
2020-04-03 17:56:46 +0100

    Conditionally run csv2rdf and skip uploading

--- vars/familyTransformPipeline.groovy
+++ vars/familyTransformPipeline.groovy
@@ -23,152 +23,40 @@ def call(body) {
                 }
             }
             stage('Transform') {
-                agent {
-                    docker {
-                        image 'gsscogs/databaker'
-                        reuseNode true
-                        alwaysPull true
-                    }
-                }
-                steps {
-                    script {
-                        FAILED_STAGE = env.STAGE_NAME
-                        ansiColor('xterm') {
-                            if (fileExists("${DATASET_DIR}/main.py")) {
-                                sh "jupytext --to notebook ${DATASET_DIR}/*.py"
-                            }
-                            sh "jupyter-nbconvert --output-dir=${DATASET_DIR}/out --ExecutePreprocessor.timeout=None --execute '${DATASET_DIR}/main.ipynb'"
-                        }
-                    }
-                }
-            }
-            stage('Validate CSV') {
-                agent {
-                    docker {
-                        image 'gsscogs/csvlint'
-                        reuseNode true
-                        alwaysPull true
-                    }
-                }
-                when {
-                    expression {
-                        def info = readJSON(text: readFile(file: "${DATASET_DIR}/info.json"))
-                        if (info.containsKey('transform') && info['transform'].containsKey('validate')) {
-                            return info['transform']['validate']
-                        } else {
-                            return true
-                        }
-                    }
-                }
-                steps {
-                    script {
-                        FAILED_STAGE = env.STAGE_NAME
-                        ansiColor('xterm') {
-                            if (fileExists("${DATASET_DIR}/schema.json")) {
-                                sh "csvlint --no-verbose -s ${DATASET_DIR}/schema.json"
-                            } else {
-                                def schemas = []
-                                for (def schema : findFiles(glob: "${DATASET_DIR}/out/*-schema.json")) {
-                                    schemas.add("${DATASET_DIR}/out/${schema.name}")
-                                }
-                                for (String schema : schemas) {
-                                    sh "csvlint --no-verbose -s '${schema}'"
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            stage('Upload Tidy Data') {
-                steps {
-                    script {
-                        FAILED_STAGE = env.STAGE_NAME
-                        jobDraft.replace()
-                        def datasets = []
-                        String dspath = util.slugise(env.JOB_NAME)
-                        def outputFiles = findFiles(glob: "${DATASET_DIR}/out/*.csv")
-                        if (outputFiles.length == 0) {
-                            error(message: "No output CSV files found")
-                        } else {
-                            for (def observations : outputFiles) {
-                                String thisPath = (outputFiles.length == 1) ? dspath : "${dspath}/${observations.name.take(observations.name.lastIndexOf('.'))}"
-                                def dataset = [
-                                        "csv"     : "${DATASET_DIR}/out/${observations.name}",
-                                        "metadata": "${DATASET_DIR}/out/${observations.name}-metadata.trig",
-                                        "path"    : thisPath
-                                ]
-                                datasets.add(dataset)
-                            }
-                        }
-                        for (def dataset : datasets) {
-                            uploadTidy([dataset.csv],
-                                    "reference/columns.csv",
-                                    dataset.path,
-                                    dataset.metadata)
-                        }
-                    }
-                }
-            }
-            stage('Test draft dataset') {
-                agent {
-                    docker {
-                        image 'gsscogs/gdp-sparql-tests'
-                        reuseNode true
-                        alwaysPull true
-                    }
-                }
-                steps {
-                    script {
-                        FAILED_STAGE = env.STAGE_NAME
-                        pmd = pmdConfig("pmd")
-                        String draftId = pmd.drafter.findDraftset(env.JOB_NAME).id
-                        String endpoint = pmd.drafter.getDraftsetEndpoint(draftId)
-                        String dspath = util.slugise(env.JOB_NAME)
-                        def dsgraphs = []
-                        if (fileExists("${DATASET_DIR}/out/observations.csv")) {
-                            dsgraphs.add("${pmd.config.base_uri}/graph/${dspath}")
-                        } else {
-                            for (def observations : findFiles(glob: "${DATASET_DIR}/out/*.csv")) {
-                                String basename = observations.name.take(observations.name.lastIndexOf('.'))
-                                dsgraphs.add("${pmd.config.base_uri}/graph/${dspath}/${basename}")
-                            }
-                        }
-                        withCredentials([usernamePassword(credentialsId: pmd.config.credentials, usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-                            for (String dsgraph : dsgraphs) {
-                                sh "sparql-test-runner -t /usr/local/tests -s ${endpoint}?union-with-live=true -a '${USER}:${PASS}' -p \"dsgraph=<${dsgraph}>\" -r 'reports/TESTS-${dsgraph.substring(dsgraph.lastIndexOf('/') + 1)}.xml'"
-                            }
-                        }
-                    }
-                }
-            }
-            stage('Draftset') {
-                parallel {
-                    stage('Submit for review') {
-                        when {
-                            expression {
-                                def info = readJSON(text: readFile(file: "${DATASET_DIR}/info.json"))
-                                if (info.containsKey('transform') && info['transform'].containsKey('review')) {
-                                    return !info['transform']['review']
-                                } else {
-                                    return false
-                                }
+                stages {
+                    stage('Tidy CSV') {
+                        agent {
+                            docker {
+                                image 'gsscogs/databaker'
+                                reuseNode true
+                                alwaysPull true
                             }
                         }
                         steps {
                             script {
                                 FAILED_STAGE = env.STAGE_NAME
-                                pmd = pmdConfig("pmd")
-                                String draftId = pmd.drafter.findDraftset(env.JOB_NAME).id
-                                pmd.drafter.submitDraftsetTo(draftId, uk.org.floop.jenkins_pmd.Drafter.Role.EDITOR, null)
+                                ansiColor('xterm') {
+                                    if (fileExists("${DATASET_DIR}/main.py")) {
+                                        sh "jupytext --to notebook ${DATASET_DIR}/*.py"
+                                    }
+                                    sh "jupyter-nbconvert --output-dir=${DATASET_DIR}/out --ExecutePreprocessor.timeout=None --execute '${DATASET_DIR}/main.ipynb'"
+                                }
                             }
                         }
                     }
-                    stage('Publish') {
+                    stage('Validate CSV') {
+                        agent {
+                            docker {
+                                image 'gsscogs/csvlint'
+                                reuseNode true
+                                alwaysPull true
+                            }
+                        }
                         when {
                             expression {
                                 def info = readJSON(text: readFile(file: "${DATASET_DIR}/info.json"))
-                                if (info.containsKey('transform') && info['transform'].containsKey('review')) {
-                                    return info['transform']['review']
+                                if (info.containsKey('transform') && info['transform'].containsKey('validate')) {
+                                    return info['transform']['validate']
                                 } else {
                                     return true
                                 }
@@ -177,7 +65,171 @@ def call(body) {
                         steps {
                             script {
                                 FAILED_STAGE = env.STAGE_NAME
-                                jobDraft.publish()
+                                ansiColor('xterm') {
+                                    if (fileExists("${DATASET_DIR}/schema.json")) {
+                                        sh "csvlint --no-verbose -s ${DATASET_DIR}/schema.json"
+                                    } else {
+                                        def schemas = []
+                                        for (def schema : findFiles(glob: "${DATASET_DIR}/out/*-schema.json")) {
+                                            schemas.add("${DATASET_DIR}/out/${schema.name}")
+                                        }
+                                        for (String schema : schemas) {
+                                            sh "csvlint --no-verbose -s '${schema}'"
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    stage('Data Cube') {
+                        agent {
+                            docker {
+                                image 'gsscogs/csv2rdf'
+                                reuseNode true
+                                alwaysPull true
+                            }
+                        }
+                        when {
+                            expression {
+                                def info = readJSON(text: readFile(file: "${DATASET_DIR}/info.json"))
+                                return info.containsKey('transform') && info['transform'].containsKey('to_rdf')
+                            }
+                        }
+                        steps {
+                            script {
+                                FAILED_STAGE = env.STAGE_NAME
+                                def datasets = []
+                                for (def csv : findFiles(glob: "${DATASET_DIR}/out/*.csv")) {
+                                    if (fileExists("${DATASET_DIR}/out/${csv.name}-metadata.json")) {
+                                        String baseName = csv.name.take(csv.name.lastIndexOf('.'))
+                                        datasets.add([
+                                                "csv": "${DATASET_DIR}/out/${csv.name}",
+                                                "metadata": "${DATASET_DIR}/out/${csv.name}-metadata.json",
+                                                "output": "${DATASET_DIR}/out/${baseName}.ttl"
+                                        ])
+                                    }
+                                }
+                                for (def dataset : datasets) {
+                                    sh "csv2rdf -t '${dataset.csv}' -u '${dataset.metadata}' -m annotated | gzip > '${dataset.output}.gz'"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            stage('Load') {
+                when {
+                    expression {
+                        def info = readJSON(text: readFile(file: "${DATASET_DIR}/info.json"))
+                        if (info.containsKey('load') && info['load'].containsKey('skip')) {
+                            return !info['load']['skip']
+                        } else {
+                            return true
+                        }
+                    }
+                }
+                stages {
+                    stage('Tidy CSV') {
+                        steps {
+                            script {
+                                FAILED_STAGE = env.STAGE_NAME
+                                jobDraft.replace()
+                                def datasets = []
+                                String dspath = util.slugise(env.JOB_NAME)
+                                def outputFiles = findFiles(glob: "${DATASET_DIR}/out/*.csv")
+                                if (outputFiles.length == 0) {
+                                    error(message: "No output CSV files found")
+                                } else {
+                                    for (def observations : outputFiles) {
+                                        String thisPath = (outputFiles.length == 1) ? dspath : "${dspath}/${observations.name.take(observations.name.lastIndexOf('.'))}"
+                                        def dataset = [
+                                                "csv"     : "${DATASET_DIR}/out/${observations.name}",
+                                                "metadata": "${DATASET_DIR}/out/${observations.name}-metadata.trig",
+                                                "path"    : thisPath
+                                        ]
+                                        datasets.add(dataset)
+                                    }
+                                }
+                                for (def dataset : datasets) {
+                                    uploadTidy([dataset.csv],
+                                            "reference/columns.csv",
+                                            dataset.path,
+                                            dataset.metadata)
+                                }
+                            }
+                        }
+                    }
+                    stage('Test draft dataset') {
+                        agent {
+                            docker {
+                                image 'gsscogs/gdp-sparql-tests'
+                                reuseNode true
+                                alwaysPull true
+                            }
+                        }
+                        steps {
+                            script {
+                                FAILED_STAGE = env.STAGE_NAME
+                                pmd = pmdConfig("pmd")
+                                String draftId = pmd.drafter.findDraftset(env.JOB_NAME).id
+                                String endpoint = pmd.drafter.getDraftsetEndpoint(draftId)
+                                String dspath = util.slugise(env.JOB_NAME)
+                                def dsgraphs = []
+                                if (fileExists("${DATASET_DIR}/out/observations.csv")) {
+                                    dsgraphs.add("${pmd.config.base_uri}/graph/${dspath}")
+                                } else {
+                                    for (def observations : findFiles(glob: "${DATASET_DIR}/out/*.csv")) {
+                                        String basename = observations.name.take(observations.name.lastIndexOf('.'))
+                                        dsgraphs.add("${pmd.config.base_uri}/graph/${dspath}/${basename}")
+                                    }
+                                }
+                                withCredentials([usernamePassword(credentialsId: pmd.config.credentials, usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+                                    for (String dsgraph : dsgraphs) {
+                                        sh "sparql-test-runner -t /usr/local/tests -s ${endpoint}?union-with-live=true -a '${USER}:${PASS}' -p \"dsgraph=<${dsgraph}>\" -r 'reports/TESTS-${dsgraph.substring(dsgraph.lastIndexOf('/') + 1)}.xml'"
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    stage('Draftset') {
+                        parallel {
+                            stage('Submit for review') {
+                                when {
+                                    expression {
+                                        def info = readJSON(text: readFile(file: "${DATASET_DIR}/info.json"))
+                                        if (info.containsKey('transform') && info['transform'].containsKey('review')) {
+                                            return !info['transform']['review']
+                                        } else {
+                                            return false
+                                        }
+                                    }
+                                }
+                                steps {
+                                    script {
+                                        FAILED_STAGE = env.STAGE_NAME
+                                        pmd = pmdConfig("pmd")
+                                        String draftId = pmd.drafter.findDraftset(env.JOB_NAME).id
+                                        pmd.drafter.submitDraftsetTo(draftId, uk.org.floop.jenkins_pmd.Drafter.Role.EDITOR, null)
+                                    }
+                                }
+                            }
+                            stage('Publish') {
+                                when {
+                                    expression {
+                                        def info = readJSON(text: readFile(file: "${DATASET_DIR}/info.json"))
+                                        if (info.containsKey('transform') && info['transform'].containsKey('review')) {
+                                            return info['transform']['review']
+                                        } else {
+                                            return true
+                                        }
+                                    }
+                                }
+                                steps {
+                                    script {
+                                        FAILED_STAGE = env.STAGE_NAME
+                                        jobDraft.publish()
+                                    }
+                                }
                             }
                         }
                     }
