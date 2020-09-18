a48a1359c015a2907432c0d0fe42c6f84b9cf77d
Alex Tucker <alex@floop.org.uk>
2020-09-18 15:26:32 +0100

    Add SKOS tests.

--- vars/familyTransformPipeline.groovy
+++ vars/familyTransformPipeline.groovy
@@ -261,6 +261,7 @@ def call(body) {
                                 wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: TOKEN, var: 'TOKEN']]]) {
                                     sh "sparql-test-runner -i -t /usr/local/tests/qb -s '${endpoint}?union-with-live=true&timeout=180' -l 10 -k '${TOKEN}' ${fromArgs} -r 'reports/TESTS-${dspath}-qb.xml'"
                                     sh "sparql-test-runner -i -t /usr/local/tests/pmd/pmd4 -s '${endpoint}?union-with-live=true&timeout=180' -l 10 -k '${TOKEN}' ${fromArgs} -r 'reports/TESTS-${dspath}-pmd.xml'"
+                                    sh "sparql-test-runner -i -t /usr/local/tests/skos -s '${endpoint}?union-with-live=true&timeout=180' -l 10 -k '${TOKEN}' ${fromArgs} -r 'reports/TESTS-${dspath}-skos.xml'"
                                 }
                             }
                         }
