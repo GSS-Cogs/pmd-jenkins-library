0942c123ad327738d4e95794efd7db7be55894eb
Rob Barry <rob.barry@ons.gov.uk>
2020-11-11 15:29:44 +0000

    Adding provenance information to the published graph. Includes information of which info.json file was responsible for the uploaded changes.

--- src/uk/org/floop/jenkins_pmd/Job.groovy
+++ src/uk/org/floop/jenkins_pmd/Job.groovy
@@ -4,11 +4,13 @@ import org.jenkinsci.plugins.uniqueid.IdStore
 import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
 
 import java.time.Instant
+import java.util.regex.Pattern
 
 class Job {
     static String getID(RunWrapper build) {
         def job = build.rawBuild.parent
         String id = IdStore.getId(job)
+//        build.getBuildVariables().get("RepositoryName")
         if (id == null) {
             IdStore.makeId(job)
             id = IdStore.getId(job)
@@ -19,21 +21,77 @@ class Job {
     static String getPROV(RunWrapper build, String graph) {
         String jobId = getID(build)
         String generatedAt = Instant.ofEpochMilli(build.timeInMillis).toString()
+
         return """
 @prefix prov: <http://www.w3.org/ns/prov#> .
 @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
 @prefix gdp: <http://gss-data.org.uk/def/gdp#> .
 @prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
+@prefix foaf: <http://xmlns.com/foaf/0.1/> .
 
 <${graph}> prov:wasGeneratedBy <${build.absoluteUrl}> .
+
 <${build.absoluteUrl}> a prov:Activity ;
   prov:wasAssociatedWith <${build.rawBuild.parent.absoluteUrl}> ;
   prov:generatedAtTime "${generatedAt}"^^xsd:dateTime ;
-  rdfs:label "${build.fullDisplayName}" .
+  rdfs:label "${build.fullDisplayName}".
+
 <${build.rawBuild.parent.absoluteUrl}> a prov:Agent ;
   gdp:uniqueID "${jobId}" ;
   rdfs:label "${build.rawBuild.parent.fullDisplayName}" .
+
+${getInfoJsonProvidenceSparql(build)}
 """
+    }
+
+    private static String getInfoJsonProvidenceSparql(RunWrapper build) {
+        return getMaybeInfoJsonLabelAndUrl(build)
+                .map({
+                    """
+                    <${build.absoluteUrl}> prov:wasInfluencedBy [
+                        a prov:Entity;
+                        rdfs:label "${it.first}"@en;
+                        foaf:page <${it.second}>
+                      ].
+                """
+                })
+                .orElse("")
+    }
+
+    /**
+     * Returns an optional tuple containing (label, url) describing the info.json used in this build.
+     * Used to describe the providence of data.
+     * @param build
+     * @return an optional tuple containing (label, url)
+     */
+    private static Optional<Tuple2<String, String>> getMaybeInfoJsonLabelAndUrl(RunWrapper build) {
+        def buildVariables = build.getBuildVariables()
+        // See https://ci.floop.org.uk/env-vars.html/ for full list of available environmental variables.
+        String gitCommitHash = buildVariables["GIT_COMMIT"]
+        String gitRemoteUrl = buildVariables["GIT_URL"]
+                .split(",") // May be multiple listed push/pull remotes
+                .first()
+        String datasetDir = buildVariables["DATASET_DIR"]
+
+        // Expecting a gitRemoteUrl of the form:
+        // "git@github.com:GSS-Cogs/pmd-jenkins-library.git"
+        // OR "https://github.com/GSS-Cogs/family-covid-19"
+        def regex = new Pattern("^.*?GSS-Cogs/(.?)(.git)?\$")
+        def matches = regex.matcher(gitRemoteUrl)
+
+        if (!matches.matches()) {
+            error "Could not comprehend git remote URL '${gitRemoteUrl}'."
+            return Optional.empty()
+        }
+
+        String repoName = matches.group(1);
+        String repoBaseUrl = "https://github.com/GSS-Cogs/${repoName}"
+        String infoJsonAtCommitUrl = "${repoBaseUrl}/tree/${gitCommitHash}/${datasetDir}/info.json"
+        String infoJsonCommitLabel = "info.json inside ${repoName}/${datasetDir}"
+
+        return Optional.of(
+                new Tuple2<String, String>(infoJsonCommitLabel, infoJsonAtCommitUrl)
+        )
     }
 
     static List<String> graphs(RunWrapper build, PMD pmd, String draftId) {
