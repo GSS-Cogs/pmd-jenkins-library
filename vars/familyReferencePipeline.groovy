77b6636901c423282e4effc919edb896759a484a
Rob Barry <rob.barry@ons.gov.uk>
2021-02-03 16:11:58 +0000

    Don't want any historical test reports either.

--- vars/familyReferencePipeline.groovy
+++ vars/familyReferencePipeline.groovy
@@ -24,6 +24,7 @@ def call(body) {
                 steps {
                     script {
                         sh "rm -rf out"
+                        sh "rm -rf reports"
                     }
                 }
             }
